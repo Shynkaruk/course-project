8ffe577c743767c5cb7cecc8deb9581d2a4717e3
Oleh Shynkaruk <shunkarykoleg@gmail.com>
2022-06-11 01:53:10 +0300

    Add function in the Fun.h

--- Project/Fun.h
+++ Project/Fun.h
@@ -5,3 +5,5 @@ void DataEntry(Data* (&d), int& n); // введення данних
 void DataRead(Data* (&d), int& n, string file); // читання данних з файлу
 void Printer(Data* d, int n); // виведення данних в консоль
 void Change(Data* d, int n); // зміна данних
+void AddData(); // додавання данних 
+void DeleteData(); // видалення данних
